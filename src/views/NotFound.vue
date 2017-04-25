9330ce43de866c118c66c764010cc17dd9a593a3
Edward Xia <xias1@uci.edu>
2017-04-25 00:25:08 -0700

    Improve 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <h1>404 Not Found!</h1>
+  <div class="container my-5">
+    <h1 class="display-1">404</h1>
+    <p class="lead">This is not the web page you are looking for.</p>
+    <hr>
   </div>
 </template>
+
+<style scoped>
+.container {
+  max-width: 52.5em;
+}
+</style>
