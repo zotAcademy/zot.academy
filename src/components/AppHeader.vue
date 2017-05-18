2e5c66342b99cceeb3c314c93220df4be0389ffa
Edward Xia <xias1@uci.edu>
2017-05-17 22:21:53 -0700

    Use height instead of width on octicons

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -26,8 +26,8 @@
               <a class="nav-link dropdown-toggle d-inline hidden-md-down mr-2" href="#" id="navbarDropdownMenuLink" aria-haspopup="true" :aria-expanded="dropdown.expanded" @click.prevent="dropdown.expanded = !dropdown.expanded" v-on-clickaway="closeDropdown">
                 @{{ username }}
               </a>
-              <router-link class="btn btn-outline-primary py-1 px-2 hidden-md-up" to="/posts/new" v-html="octicons.comment.toSVG({width:28})"></router-link>
-              <a class="btn btn-outline-primary py-1 px-2 hidden-sm-down" href="/posts/new" v-html="octicons.comment.toSVG({width:28})" @click.prevent="compose"></a>
+              <router-link class="btn btn-outline-primary py-1 px-2 hidden-md-up" to="/posts/new" v-html="octicons.comment.toSVG({height:28})"></router-link>
+              <a class="btn btn-outline-primary py-1 px-2 hidden-sm-down" href="/posts/new" v-html="octicons.comment.toSVG({height:28})" @click.prevent="compose"></a>
               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
                 <router-link class="dropdown-item" :to="'/' + username">View profile</router-link>
                 <div class="dropdown-divider"></div>
