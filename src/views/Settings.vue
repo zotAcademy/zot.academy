0b3d46113f69db9059c6dff7b64c4f88a8b33141
kcmin <kcmin@uci.edu>
2017-05-19 15:34:57 -0700

    settings

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,6 +1,39 @@
 <template>
-  <div class="container my-3">
-    <h1>Settings</h1>
+  <div class="container my-5">
+
+    <div class="card my-3">
+      <div class="card-block">
+        <p class="card-text"> Want another account? <router-link to="/signup">Create an account</router-link>.</p>
+      </div>
+    </div>
+
+
+    <div class = "card my-3">
+      <div class="card-block">
+        <h4 class="card-title">Edit Login Info</h4>
+
+        <div class="form-group">
+          <p> New username? </p>
+          <input type="text" class="form-control" placeholder = "New username">
+        </div>
+
+        <div class="form-group">
+          <p> New email? </p>
+          <input type="text" class="form-control" placeholder = "New email">
+        </div>
+
+        <div class="form-group">
+          <p> New password? </p>
+          <input type="text" class="form-control" placeholder = "New password" max = "254">
+        </div>
+
+        <button class="btn btn-block btn-primary" > Submit Changes <router-link to="/home"> </router-link> </button>
+
+      </div>
+
+
+    </div>
+
   </div>
 </template>
 
@@ -17,4 +50,7 @@ export default {
 </script>
 
 <style scoped>
+.container {
+  max-width: 430px;
+}
 </style>
