a1a6bce208857f8f5dfddea04f816381265a1bee
Edward Xia <xias1@uci.edu>
2017-06-07 01:26:46 -0700

    Add mobile reply

---
+++ src/views/ReplyPost.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="card my-3">
+      <div class="card-header text-center">
+        {{ title }}
+      </div>
+      <div class="card-block">
+        <app-post-composer></app-post-composer>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import AppPost from '@/components/AppPost'
+import AppPostComposer from '@/components/AppPostComposer'
+
+export default {
+  name: 'reply-post',
+  props: ['id'],
+  components: {
+    AppPost,
+    AppPostComposer
+  },
+  computed: {
+    ...mapGetters({
+      getPostById: 'posts/getPostById'
+    }),
+    post () {
+      return this.getPostById(this.id)
+    },
+    title () {
+      if (this.post != null) {
+        return 'Reply to @' + this.post.user.username
+      }
+      return 'Reply'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
