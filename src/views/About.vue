9643c5cd718d212248d7c17ada0b120f19029b50
jessicmz <jessicmz@uci.edu>
2017-06-11 23:17:22 -0700

    About Page Final Edits
    - Changed descriptions
    - added icons for social media
    - small margin changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -23,11 +23,20 @@
         <div class="container-team">
           <div class="row">
             <div class="col col-12 col-md-6 col-lg-4">
-              <a href="https://www.linkedin.com/in/jessica-zhang-96955b103/">
-                <img class="profile-image" src="../assets/edward.jpg" alt="Smiley face">
-              </a>
+              <img class="profile-image" src="../assets/edward.jpg" alt="Smiley face">
               <p class="lead team-names"> Edward Xia </p>
               <p class="team-role"> Lead Developer </p>
+              <div class="contact">
+                <a href="https://www.linkedin.com/in/ntkme/">
+                  <img class="contact-image" src="../assets/LinkedIn.png" alt="LinkedIn">
+                </a>
+                <a href="https://www.facebook.com/natsuki.natsume">
+                  <img class="contact-image" src="../assets/Facebook.png" alt="Facebook">
+                </a>
+                <a href="mailto:xias1@uci.edu" target="_top">
+                  <img class="contact-image" src="../assets/Email.png" alt="Email">
+                </a>
+              </div>
               <p class="team-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -35,11 +44,20 @@
               </p>
             </div>
             <div class="col col-12 col-md-6 col-lg-4 ">
-              <a href="https://www.linkedin.com/in/jessica-zhang-96955b103/">
-                <img class="profile-image" src="../assets/juno.jpg" alt="Smiley face">
-              </a>
+              <img class="profile-image" src="../assets/juno.jpg" alt="Smiley face">
               <p class="lead team-names"> Juno Hyun </p>
               <p class="team-role"> Lead Developer </p>
+              <div class="contact">
+                <a href="https://www.linkedin.com/in/samueljhyun/">
+                  <img class="contact-image" src="../assets/LinkedIn.png" alt="LinkedIn">
+                </a>
+                <a href="https://www.facebook.com/JUNOBONO">
+                  <img class="contact-image" src="../assets/Facebook.png" alt="Facebook">
+                </a>
+                <a href="mailto:sjhyun@uci.edu" target="_top">
+                  <img class="contact-image" src="../assets/Email.png" alt="Email">
+                </a>
+              </div>
               <p class="team-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -47,11 +65,20 @@
               </p>
             </div>
             <div class="col col-12 col-md-6 col-lg-4">
-              <a href="https://www.linkedin.com/in/jessica-zhang-96955b103/">
-                <img class="profile-image" src="../assets/idan.jpg" alt="Smiley face">
-              </a>
-              <p class="lead team-names"> Idan Imaleh </p>
+              <img class="profile-image" src="../assets/idan.jpg" alt="Smiley face">
+              <p class="lead team-names"> Idan Elmaleh </p>
               <p class="team-role"> Developer </p>
+              <div class="contact">
+                <a href="https://www.linkedin.com/in/idan-elmaleh-0b7345108/">
+                  <img class="contact-image" src="../assets/LinkedIn.png" alt="LinkedIn">
+                </a>
+                <a href="https://www.facebook.com/idanelmaleh7">
+                  <img class="contact-image" src="../assets/Facebook.png" alt="Facebook">
+                </a>
+                <a href="mailto:ielmaleh@uci.edu" target="_top">
+                  <img class="contact-image" src="../assets/Email.png" alt="Email">
+                </a>
+              </div>
               <p class="team-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -59,39 +86,57 @@
               </p>
             </div>
             <div class="col col-12 col-md-6 col-lg-4">
-              <a href="https://www.linkedin.com/in/jessica-zhang-96955b103/">
-                <img class="profile-image" src="../assets/michael.jpg" alt="Smiley face">
-              </a>
+              <img class="profile-image" src="../assets/michael.jpg" alt="Smiley face">
               <p class="lead team-names"> Michael Min </p>
               <p class="team-role">  Developer </p>
-              <p class="team-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
+              <div class="contact">
+                <a href="https://www.linkedin.com/in/michael-min-7a39a098/">
+                  <img class="contact-image" src="../assets/LinkedIn.png" alt="LinkedIn">
+                </a>
+                <a href="https://www.facebook.com/michael.min.92">
+                  <img class="contact-image" src="../assets/Facebook.png" alt="Facebook">
+                </a>
+                <a href="mailto:kcmin@uci.edu" target="_top">
+                  <img class="contact-image" src="../assets/Email.png" alt="Email">
+                </a>
+              </div>
+              <p class="team-desc">Hey my name is Michael and I like corgis. I have a little booger named Roi who likes to get pats on the head. You can often find me taking a walk on the beach with my dog, or napping at the library!
               </p>
             </div>
             <div class="col col-12 col-md-6 col-lg-4">
-              <a href="https://www.linkedin.com/in/jessica-zhang-96955b103/">
-                <img class="profile-image" src="../assets/andrew.jpg" alt="Smiley face">
-              </a>
+              <img class="profile-image" src="../assets/andrew.jpg" alt="Smiley face">
               <p class="lead team-names"> Andrew Le </p>
               <p class="team-role">  Developer </p>
-              <p class="team-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
+              <div class="contact">
+                <a href="https://www.linkedin.com/in/andrew-le-164325133/">
+                  <img class="contact-image" src="../assets/LinkedIn.png" alt="LinkedIn">
+                </a>
+                <a href="https://www.facebook.com/andrewthechino">
+                  <img class="contact-image" src="../assets/Facebook.png" alt="Facebook">
+                </a>
+                <a href="mailto:leaa1@uci.edu" target="_top">
+                  <img class="contact-image" src="../assets/Email.png" alt="Email">
+                </a>
+              </div>
+              <p class="team-desc">Hi! My name is Andrew. I am quite new to the world of computer science and programming. With this fresh mind, I am eager to learn new technologies and programming languages to apply to my own projects. With coding as legos, I am ready to build anything.
               </p>
             </div>
             <div class="col col-12 col-md-6 col-lg-4">
-              <a href="https://www.linkedin.com/in/jessica-zhang-96955b103/">
-                <img class="profile-image" src="../assets/jessica.jpg" alt="Smiley face">
-              </a>
+              <img class="profile-image" src="../assets/jessica.jpg" alt="Smiley face">
               <p class="lead team-names"> Jessica Zhang </p>
               <p class="team-role">  Developer </p>
-              <p class="team-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
+              <div class="contact">
+                <a href="https://www.linkedin.com/in/jessica-zhang-96955b103/">
+                  <img class="contact-image" src="../assets/LinkedIn.png" alt="LinkedIn">
+                </a>
+                <a href="https://www.facebook.com/jessica.zhang.35">
+                  <img class="contact-image" src="../assets/Facebook.png" alt="Facebook">
+                </a>
+                <a href="mailto:jessicmz@uci.edu" target="_top">
+                  <img class="contact-image" src="../assets/Email.png" alt="Email">
+                </a>
+              </div>
+              <p class="team-desc">I am a fourth year undergraduate attending the University of California, Irvine studying Software Engineering.  I believe software development and user experience are both important, interdependent and inseparable roles that produce user-centered solutions.
               </p>
             </div>
           </div>
@@ -103,7 +148,6 @@
 
 <script>
 import octicons from 'octicons'
-
 export default {
   name: 'about',
   data () {
@@ -122,27 +166,33 @@ export default {
   display: block;
   margin-left: auto;
   margin-right: auto;
+
 }
+
 span{
   float:left;
+}
+.contact{
+  text-align: center;
+}
+.contact-image{
+  max-width: 10%;
 }
 .col col-12 col-md-6 col-lg-4{
   margin-bottom: 4em;
 }
 .team-names{
   text-align: center;
-  margin: 0 0 0 0;
+  margin: 1em 0 0 0;
 }
-
 .team-role{
   text-align: center;
   color: #647F92;
 }
 .team-desc{
+  margin: 1em 0 1em 0;
   padding-left: 3em;
 }
-
-
 .container{
   padding: 2em 0 0 0;
   max-width: none;
@@ -155,7 +205,6 @@ span{
 .container-text{
   background-color: #f7f7f8;
 }
-
 .vision-title{
   font-size: 200%;
   text-align: center;
@@ -165,11 +214,9 @@ span{
   font-size: 200%;
   text-align: center;
 }
-
 .vision{
   font-size: 120%;
 }
-
 .vision-text{
   font-size: 120%;
 }
